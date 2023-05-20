d1677831b0d51c0746a0fb8a373230b0b48f648f
andreynaz4renko <andrey.naz4renko@gmail.com>
2023-05-20 13:07:42 +0300

    added counter field in ViewModel

--- app/src/main/java/com/andreynaz4renko/masterapplication/ui/MainViewModel.kt
+++ app/src/main/java/com/andreynaz4renko/masterapplication/ui/MainViewModel.kt
@@ -1,7 +1,10 @@
 package com.andreynaz4renko.masterapplication.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    val counter = MutableLiveData<Int>()
 
 }
